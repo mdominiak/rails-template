bfed4bb4d80ad503fe9a89e4e6cb6e60af39657e
Matt Dominiak <mateusz.dominiak@gmail.com>
2022-07-21 09:17:43 +0200

    Sign up system test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,19 +3,35 @@ require "application_system_test_case"
 class UsersTest < ApplicationSystemTestCase
   test "login and logout" do
     User.create!(
-      email: 'bob@gmail.com',
+      email: 'matt@gmail.com',
       password: 'password'
     )
 
     visit root_url
     click_on 'Log in'
-    fill_in 'Email', with: 'bob@gmail.com'
+    fill_in 'Email', with: 'matt@gmail.com'
     fill_in 'Password', with: 'password'
     click_on 'Log in'
-
     assert_selector "h1", text: "Home#index"
 
     click_on 'Log out'
     assert_selector 'a', text: 'Log in'
+  end
+
+  test "sign up and login" do
+    visit root_url
+    click_on 'Sign up'
+    fill_in 'Email', with: 'matt@gmail.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_on 'Sign up'
+    assert_selector "h1", text: "Home#index"
+
+    click_on 'Log out'
+    click_on 'Log in'
+    fill_in 'Email', with: 'matt@gmail.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+    assert_selector "h1", text: "Home#index"
   end
 end

7cdf6a1c4bae1ed980b66db3a27bf5a4a8644174
Matt Dominiak <mateusz.dominiak@gmail.com>
2022-11-17 20:33:57 +0100

    Update devise_turbo_controller.rb

--- app/controllers/devise_turbo_controller.rb
+++ app/controllers/devise_turbo_controller.rb
@@ -1,14 +1,32 @@
 class DeviseTurboController < ApplicationController
+  # class Responder < ActionController::Responder
+  #   def to_turbo_stream
+  #     controller.render(options.merge(formats: :html))
+  #   rescue ActionView::MissingTemplate => error
+  #     if get?
+  #       raise error
+  #     elsif has_errors? && default_action
+  #       render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+  #     else
+  #       redirect_to navigation_location
+  #     end
+  #   end
+  # end
+
   class Responder < ActionController::Responder
     def to_turbo_stream
-      controller.render(options.merge(formats: :html))
+      if @default_response
+        @default_response.call(options.merge(formats: :html))
+      else
+        controller.render(options.merge(formats: :html))
+      end
     rescue ActionView::MissingTemplate => error
       if get?
         raise error
       elsif has_errors? && default_action
         render rendering_options.merge(formats: :html, status: :unprocessable_entity)
       else
-        redirect_to navigation_location
+        navigation_behavior error
       end
     end
   end
